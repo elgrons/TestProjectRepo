2f0f7369068db8b20388a5f020184538b5334677
Eliot Gronstal <eliot.lauren@gmail.com>
2023-06-13 10:02:13 -0700

    server connected

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 import dotenv from "dotenv";
-import {App} from "octokit";
-import {createNodeMiddleware} from "@octokit/webhooks";
+import { App } from "octokit";
+import { createNodeMiddleware } from "@octokit/webhooks";
 import fs from "fs";
 import http from "http";
 
@@ -20,29 +20,37 @@ const app = new App({
   appId: appId,
   privateKey: privateKey,
   webhooks: {
-    secret: webhookSecret
+    secret: webhookSecret,
   },
 });
 
 //This code adds an event handler that you will call later. When this event handler is called, it will log the event to the console. Then, it will use GitHub's REST API to add a comment to the pull request that triggered the event.
-const messageForNewPRs = "Thanks for opening a new PR! Please follow our contributing guidelines to make your PR easier to review.";
+const messageForNewPRs =
+  "Thanks for opening a new PR! Please follow our contributing guidelines to make your PR easier to review.";
 
-async function handlePullRequestOpened({octokit, payload}) {
-  console.log(`Received a pull request event for #${payload.pull_request.number}`);
+async function handlePullRequestOpened({ octokit, payload }) {
+  console.log(
+    `Received a pull request event for #${payload.pull_request.number}`
+  );
 
   try {
-    await octokit.request("POST /repos/{owner}/{repo}/issues/{issue_number}/comments", {
-      owner: payload.repository.owner.login,
-      repo: payload.repository.name,
-      issue_number: payload.pull_request.number,
-      body: messageForNewPRs,
-      headers: {
-        "x-github-api-version": "2022-11-28",
-      },
-    });
+    await octokit.request(
+      "POST /repos/{owner}/{repo}/issues/{issue_number}/comments",
+      {
+        owner: payload.repository.owner.login,
+        repo: payload.repository.name,
+        issue_number: payload.pull_request.number,
+        body: messageForNewPRs,
+        headers: {
+          "x-github-api-version": "2022-11-28",
+        },
+      }
+    );
   } catch (error) {
     if (error.response) {
-      console.error(`Error! Status: ${error.response.status}. Message: ${error.response.data.message}`);
+      console.error(
+        `Error! Status: ${error.response.status}. Message: ${error.response.data.message}`
+      );
     }
     console.error(error);
   }
@@ -62,13 +70,13 @@ app.webhooks.onError((error) => {
 
 //to determine where your server will listen
 const port = 3000;
-const host = 'localhost';
+const host = "localhost";
 const path = "/api/webhook";
 const localWebhookUrl = `http://${host}:${port}${path}`;
 //For local development, your server will listen to port 3000 on localhost. When you deploy your app, you will change these values.
 
 //set up a middleware function to handle incoming webhook events
-const middleware = createNodeMiddleware(app.webhooks, {path});
+const middleware = createNodeMiddleware(app.webhooks, { path });
 
 //The resulting middleware function will:
 
@@ -76,9 +84,8 @@ const middleware = createNodeMiddleware(app.webhooks, {path});
 // *Parse the webhook event payload and identify the type of event.
 // *Trigger the corresponding webhook event handler.
 
-
 //This code creates a Node.js server that listens for incoming HTTP requests (including webhook payloads from GitHub) on the specified port. When the server receives a request, it executes the middleware function that you defined earlier. Once the server is running, it logs messages to the console to indicate that it is listening.
 http.createServer(middleware).listen(port, () => {
   console.log(`Server is listening for events at: ${localWebhookUrl}`);
-  console.log('Press Ctrl + C to quit.');
+  console.log("Press Ctrl + C to quit.");
 });
